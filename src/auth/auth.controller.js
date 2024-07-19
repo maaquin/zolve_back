243696714bdae7eecee4f13c741520433af5e66e
Luciano Maquin <lmaquin-2019274@kinal.edu.gt>
2024-07-19 07:19:38 -0600

    Update auth.controller.js

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,39 +1,98 @@
-import SibApiV3Sdk from 'sib-api-v3-sdk';
+import User from "../users/user.model.js";
+import bcryptjs from "bcryptjs";
+import { generarJWT } from "../helpers/generate-JWT.js";
+import { sendConfirmationEmail } from '../helpers/email-service.js'
+import crypto from "crypto";
 
-export const sendConfirmationEmail = async (email, confirmationToken, username) => {
+export const register = async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
 
-    const defaultClient = SibApiV3Sdk.ApiClient.instance;
+    const userExist = await User.find({ email: email });
 
-    // Configuración de la API key
-    const apiKey = defaultClient.authentications['api-key'];
-    apiKey.apiKey = process.env.BREVO_API_KEY;
+    if(userExist.length > 0){
+      return res.status(500).send("Correo ya registrado");
+    }
 
-    const sendEmailWithTemplate = async (toEmail, toName, templateId, params) => {
-        const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    const salt = bcryptjs.genSaltSync();
+    const encryptedPassword = bcryptjs.hashSync(password, salt);
+    const confirmationToken = crypto.randomBytes(20).toString('hex');
 
-        const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-        sendSmtpEmail.to = [{ email: toEmail, name: toName }];
-        sendSmtpEmail.templateId = templateId;
-        sendSmtpEmail.params = params;
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      confirmationToken,
+      isConfirmed: false,
+    });
 
-        try {
-            const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-            console.log('Correo enviado correctamente:', data);
-        } catch (error) {
-            console.error('Error al enviar el correo:', error);
-        }
-    };
+    await sendConfirmationEmail(email, confirmationToken, username);
 
-    // Uso de la función para enviar un correo
-    (async () => {
-        const toEmail = email;
-        const toName = username;
-        const templateId = 1;
-        const params = {
-            email: username,
-            confirmationToken: confirmationToken
-        };
+    return res.status(200).json({
+      msg: "Usuario registrado. Revisa tu correo para confirmar tu cuenta.",
+      userDetails: {
+        user: user.username,
+        email: user.email,
+        id: user._id,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send("No se pudo registrar el usuario");
+  }
+};
 
-        await sendEmailWithTemplate(toEmail, toName, templateId, params);
-    })();
-}
+export const confirmToken = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const user = await User.findOne({ confirmationToken: token });
+
+    if (!user) {
+      return res.status(400).send({ error: 'Token inválido.' });
+    }
+
+    user.isConfirmed = true;
+    user.confirmationToken = undefined;
+    await user.save();
+
+    res.send({ message: 'Cuenta confirmada con éxito.' });
+  } catch (error) {
+    res.status(500).send({ error: 'Error confirmando cuenta.' });
+  }
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //verificar si el email existe:
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if(user && (await bcryptjs.compare(password, user.password))){
+      const token = await generarJWT(user.id, user.email)
+
+      res.status(200).json({
+        msg: "Login Ok!!!",
+        userDetails: {
+          user: user.username,
+          email: user.email,
+          id: user._id
+        },
+      });
+    }
+
+    if (!user) {
+      return res
+        .status(400)
+        .send(`Wrong credentials, ${email} doesn't exists en database`);
+    }
+
+    // verificar la contraseña
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).send("wrong password");
+    }
+
+  } catch (e) {
+    res.status(500).send("Comuniquese con el administrador");
+  }
+};
